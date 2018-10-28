f17e60fcac512ef45d973b6e70425bb19a04d791
Dhawal Singh <dhawal.singh04@gmail.com>
2018-10-28 18:36:23 +0530

    redux-persist implementation

--- App.js
+++ App.js
@@ -7,30 +7,23 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
 import {Provider} from 'react-redux'
-import configureStore from './js/store'
+import {PersistGate} from 'redux-persist/lib/integration/react';
+import {store, persistor} from './js/store';
 import AppContainer from './js/containers/AppContainer'
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 type Props = {};
 
-const store = configureStore();
-
 export default class App extends Component<Props> {
-  render() {
-    return (
-        <Provider store={store}>
-            <AppContainer/>
-        </Provider>
-    )
-  }
+    render() {
+        return (
+            <Provider store={store}>
+                <PersistGate persistor={persistor}>
+                    <AppContainer/>
+                </PersistGate>
+            </Provider>
+        )
+    }
 }
 
 
--- js/store.js
+++ js/store.js
@@ -1,8 +1,22 @@
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk'
 import rootReducer from './reducers/index'
+import {persistStore, persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 
-export default function configureStore() {
-    let store = createStore(rootReducer,applyMiddleware(thunk));
-    return store
-}
+
+
+    const persistConfig = {
+        key: 'root',
+        storage: storage,
+        persistReconciler: autoMergeLevel2,
+        whitelist: ['dataReducer']
+    };
+
+    const pReducer = persistReducer(persistConfig, rootReducer);
+
+
+    export const store = createStore(pReducer,applyMiddleware(thunk));
+    export const persistor = persistStore(store);
+
